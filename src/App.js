ad06f871a39bafc5d049f8d64896dda85eb17b42
jasonconte999 <39284732+jasonconte999@users.noreply.github.com>
2020-06-20 16:26:02 -0400

    Basic template copied from profs example

---
+++ src/App.js
@@ -0,0 +1,21 @@
+/* New cleaned up version of App.js */
+import React from 'react';
+
+// Importing react-router-dom to use the React Router
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import './App.css';
+
+class App extends React.Component {
+
+  state = {
+    abc: "123"
+  }
+
+  render() {
+    return (
+
+    );  
+  }
+}
+
+export default App;
---
+++ src/react_components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./styles.css";
+
+/* The Header Component */
+class Header extends React.Component {
+  render() {
+  }
+}
+
+export default Header;
