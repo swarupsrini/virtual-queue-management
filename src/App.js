4fb0258d5fab41bcf878dba9e3af40788976ce17
Hemant Bhanot <hemant.bhanot01@gmail.com>
2020-06-24 01:19:16 -0400

    created button component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import PrimaryButton from './components/PrimaryButton';
+
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello there</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Hello there</h1>
+			<PrimaryButton></PrimaryButton>
+		</div>
+	);
 }
 
 export default App;
