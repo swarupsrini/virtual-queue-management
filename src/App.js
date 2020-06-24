8a6255cd8d6f437d49bdc83c9842583efe51910f
Hemant Bhanot <hemant.bhanot01@gmail.com>
2020-06-24 01:22:23 -0400

    fixed formatting

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
-import PrimaryButton from './components/PrimaryButton';
+import React from "react";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Hello there</h1>
-			<PrimaryButton></PrimaryButton>
 		</div>
 	);
 }
