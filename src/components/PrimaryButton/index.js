9072478f0041dccd7518ff37cd66c5eb6fbf52af
Hemant Bhanot <hemant.bhanot01@gmail.com>
2020-06-24 02:20:30 -0400

    finished primary/secondary buttons

--- src/components/PrimaryButton/index.js
+++ src/components/PrimaryButton/index.js
@@ -1,20 +1,9 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
-import { ThemeProvider } from '@material-ui/styles';
-import { createMuiTheme } from '@material-ui/core/styles';
 
-const theme = createMuiTheme({
-	palette: {
-		primary: {
-			main: "#4455BB",
-		},
-	},
-});
 
 export default function PrimaryButton(props) {
 	return (
-		<ThemeProvider theme={theme}>
-			<Button color="primary" variant="contained" onClick={props.onClick}>{props.text}</Button>
-		</ThemeProvider>
+		<Button color="primary" variant="contained" onClick={props.onClick}>{props.text}</Button>
 	);
 }
