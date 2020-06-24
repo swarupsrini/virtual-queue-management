9848c8705f9810ebc0d9ebadc68ad5f2d46c9c89
Hemant Bhanot <hemant.bhanot01@gmail.com>
2020-06-24 03:36:29 -0400

    finished save button

--- src/components/PrimaryButton/index.js
+++ src/components/PrimaryButton/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 
-
 export default function PrimaryButton(props) {
 	return (
 		<Button color="primary" variant="contained" onClick={props.onClick}>{props.text}</Button>
