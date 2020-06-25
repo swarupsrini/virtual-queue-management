e92d6fc018fa6bb5278201f15c003a76592e8668
Hemant Bhanot <hemant.bhanot01@gmail.com>
2020-06-25 15:18:33 -0400

    added simple appbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,18 +4,41 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
+	},
+	one: {
+		marginLeft: theme.spacing(2),
+	},
+}));
 
 export default function Header(props) {
+	const classes = useStyles();
 	return (
-		<AppBar position="static">
+		<AppBar position="fixed">
 			<Toolbar variant="dense">
-				<IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-					<MenuIcon />
-				</IconButton>
 				<Typography variant="h6" color="inherit">
-					Photos
-    </Typography>
+					Store Search
+    		</Typography>
+				<Typography variant="h6" color="inherit" className={classes.one}>
+					Store Search
+    		</Typography>
+				<IconButton
+					aria-label="account of current user"
+					aria-controls="menu-appbar"
+					aria-haspopup="true"
+					onClick={console.log("clicked account")}
+					color="inherit"
+				>
+					<AccountCircle />
+				</IconButton>
 			</Toolbar>
-		</AppBar>);
+		</AppBar>
+	);
 }
