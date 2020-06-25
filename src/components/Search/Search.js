1ea7a549f4e59e09e62d881bbb80449a49adbcae
jasonconte999 <39284732+jasonconte999@users.noreply.github.com>
2020-06-25 14:55:31 -0400

    Fixed callback functions

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,8 +1,8 @@
 /* 
 Search: Used to represent a search bar, with filtering capabilities
 
-filterClick: filterClick call-back function (not yet implemented)
-searchClick: searchClick call-back function (not yet implemented)
+filter: filterClick call-back function
+search(query): search call-back function. Takes contents of search bar as parameter
 
 Example: <Search filterClick= {filterClick}, searchClick = {searchClick}/>
 */
@@ -15,27 +15,25 @@ import SearchIcon from '@material-ui/icons/Search';
 import FilterList from '@material-ui/icons/FilterList';
 
 export default function Search(props) {
-  /*
-  const [favorited, setFavorited] = useState(true);
   
-  function favoriteClick(e) {
-    setFavorited(!favorited);
+  let state={
+    text:""
   }
-  
-  state={}
 
   function updateSearch(e){
-    this.setState({text:e.target.value})
-    console.log("a")
-  }*/
+    state.text = e.target.value
+  }
+  function searchClick(e){
+    props.search(state.text)
+  }
   
   return (
     <div>
-      <TextField label="Search" variant="outlined" size="small" /*onChange={this.updateSearch}*/></TextField>
-      <IconButton>
+      <TextField label="Search" variant="outlined" size="small" onChange={updateSearch}></TextField>
+      <IconButton onClick={searchClick}>
         <SearchIcon size="medium"></SearchIcon>
       </IconButton>
-      <IconButton>
+      <IconButton onClick={props.filter}>
         <FilterList size="medium"></FilterList>
       </IconButton>
     </div>
