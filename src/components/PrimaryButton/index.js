d65f00022b5edce523f43def633bb011abc33938
Hemant Bhanot <hemant.bhanot01@gmail.com>
2020-06-25 15:09:25 -0400

    updated with examples

--- src/components/PrimaryButton/index.js
+++ src/components/PrimaryButton/index.js
@@ -1,3 +1,15 @@
+/*
+Primary Button: Used to represent Buttons with primary functionality in the app
+
+text: text to be displayed in the primary button
+onClick: Called when button is clicked
+
+Example: <PrimaryButton 
+			text="dwd Kenobi" 
+			onClick={() => { console.log("hello") }}>
+		</PrimaryButton><br></br>
+
+*/
 import React from 'react';
 import Button from '@material-ui/core/Button';
 
--- src/components/SaveButton/index.js
+++ src/components/SaveButton/index.js
@@ -1,7 +1,16 @@
+/*
+Secondary Button: Used to represent Buttons that save the current state of the app (info such as passwords, email etc)
+
+onClick: Called when save button is clicked
+
+Example: <SaveButton 
+			onClick={() => { console.log("save clicked") }}>
+		</SaveButton>
+
+*/
+
 import React from 'react';
 import Button from '@material-ui/core/Button';
-
-
 import { makeStyles } from '@material-ui/core/styles';
 import { lightGreen } from '@material-ui/core/colors';
 
