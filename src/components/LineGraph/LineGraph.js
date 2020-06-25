aeccb792cc70fca1f42e7d546ab9e76c68b0a618
jasonconte999 <39284732+jasonconte999@users.noreply.github.com>
2020-06-25 16:08:17 -0400

    Simplified line graph input parameters. Now just have to give the start time, and a list of y values for the graph

--- src/components/LineGraph/LineGraph.js
+++ src/components/LineGraph/LineGraph.js
@@ -1,25 +1,14 @@
 /* 
 LineGraph: Used to represent a line graph of the rate of user entry
 
-dataPoints: array of x,y values to be plotted
+yValues: number of people at each hour the store is open
+startTime: hour store opens (24 hour format ie/ instead of 1pm, pass 13)
 
-Example: <LineGraph dataPoints= {
-	[
-		{ x: new Date(Date.UTC (2012, 1, 1, 13,0)), y: 8 },
-		{ x: new Date(Date.UTC (2012, 1, 1, 14,0)), y: 8 },
-		{ x: new Date(Date.UTC (2012, 1, 1, 15,0)), y: 8 },
-		{ x: new Date(Date.UTC (2012, 1, 1, 16,0)), y: 15 },
-		{ x: new Date(Date.UTC (2012, 1, 1, 17,0)), y: 30 },
-		{ x: new Date(Date.UTC (2012, 1, 1, 18,0)), y: 30 },
-		{ x: new Date(Date.UTC (2012, 1, 1, 19,0)), y: 28 },
-		{ x: new Date(Date.UTC (2012, 1, 1, 20,0)), y: 25 },
-		{ x: new Date(Date.UTC (2012, 1, 1, 21,0)), y: 33 },
-		{ x: new Date(Date.UTC (2012, 1, 1, 22,0)), y: 18 },
-		{ x: new Date(Date.UTC (2012, 1, 1, 23,0)), y: 10 },
-		{ x: new Date(Date.UTC (2012, 1, 1, 24,0)), y: 10 },
-		{ x: new Date(Date.UTC (2012, 1, 1, 25,0)), y: 5 },
-	]}
-	/>
+Example: 
+<LineGraph 
+	yValues= {[8,8,8,15,30,30,28,25,33,18,10,10,5]}
+	startTime = {8}
+/>
 */
 
 import React from 'react';
@@ -30,6 +19,12 @@ var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 //class LineGraph extends React.Component {
 export default function LineGraph(props) {
+	var i;
+	let dataPoints = []
+	for (i = 0; i < props.yValues.length; i++) {
+		dataPoints.push({x:new Date(Date.UTC (2012, 1, 1, props.startTime + i + 5,0)),y:props.yValues[i]})
+	}
+
 	const options = {
 		animationEnabled: true,
 		theme: "light2", // "light1", "dark1", "dark2"
@@ -46,9 +41,11 @@ export default function LineGraph(props) {
 		data: [{
 			type: "line",
 			toolTipContent: "{y} people",
-			dataPoints: props.dataPoints
+			dataPoints: dataPoints
 		}]
 	}
+	
+	
 	return (
 	<div>
 		<CanvasJSChart options = {options}/>
