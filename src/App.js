b13ecb90a09e68198ebfcc9646beb22590570e30
Swarup Srini <gswarup2001@gmail.com>
2020-06-30 16:07:31 -0400

    Created login, signup pages and updated App.js to route to them

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 
+// importing all the pages
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+
 import "./App.css";
 
 function App() {
 	return (
 		<div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' render={() => 
+                          (<LoginPage/>)}/>
+          <Route exact path='/signup' render={() => 
+                          (<SignupPage/>)}/>
+        </Switch>
+      </BrowserRouter>
 		</div>
 	);
 }
