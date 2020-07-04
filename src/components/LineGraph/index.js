03232c8182760863cf3eadd5b078a58722f7ba91
Swarup Srini <gswarup2001@gmail.com>
2020-07-03 21:58:30 -0400

    removed packages

--- src/components/LineGraph/index.js
+++ src/components/LineGraph/index.js
@@ -17,10 +17,7 @@ import React from 'react';
 import CanvasJSReact from './CanvasJS/canvasjs.react';
 var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
-// import { CanvasJS } from 'canvasjs';
-// let CanvasJSChart = CanvasJS.Chart;
 
-//class LineGraph extends React.Component {
 export default function LineGraph(props) {
 	var i;
 	let dataPoints = []
