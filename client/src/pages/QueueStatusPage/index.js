f0fff9f38d6d2a017a9fc8fd75367a62c2579b81
Hemant Bhanot <hemant.bhanot01@gmail.com>
2020-08-07 04:26:22 -0400

    almost done queue status

--- client/src/pages/QueueStatusPage/index.js
+++ client/src/pages/QueueStatusPage/index.js
@@ -2,11 +2,10 @@ import React from "react";
 import StoreHeader from "../../components/StoreHeader";
 import DataCard from "../../components/DataCard";
 import { Frame } from "framer";
-import Grid from "@material-ui/core/Grid";
 import defaultQRCode from "../../images/qrcode.png";
 import Header from "../../components/Header";
 import { Link } from "react-router-dom";
-import { Button } from "@material-ui/core";
+import { Button, Grid, Paper, Typography } from "@material-ui/core";
 import useStyles from "./styles";
 
 export default function QueueStatus(props) {
@@ -37,6 +36,50 @@ export default function QueueStatus(props) {
       >
         Exit Queue
       </Button>
+      <Paper className={classes.paper}>
+        <Grid container spacing={1} className={classes.gridContainer}>
+          <Grid item>
+            <div className={classes.divElem}>
+              <Typography variant="h6">Forecast Wait</Typography>
+              <Typography variant="h2" className={classes.typeSubtitle}>
+                45
+              </Typography>
+              <Typography variant="h5" className={classes.typeSubtitle}>
+                min
+              </Typography>
+            </div>
+          </Grid>
+          <Grid item>
+            <div className={classes.divElem}>
+              <Typography variant="h6">In Queue</Typography>
+              <Typography variant="h2" className={classes.typeSubtitle}>
+                45
+              </Typography>
+              <Typography
+                variant="h5"
+                className={classes.typeSubtitle}
+              ></Typography>
+            </div>
+          </Grid>
+          <Grid item>
+            <div className={classes.divElem}>
+              <Typography variant="h6">In Store</Typography>
+              <Typography variant="h2" className={classes.typeSubtitle}>
+                45
+              </Typography>
+              <Typography
+                variant="h5"
+                className={classes.typeSubtitle}
+              ></Typography>
+            </div>
+          </Grid>
+        </Grid>
+      </Paper>
+      <Paper className={classes.paper2}>
+        <Typography variant="h6" className={classes.message}>
+          Message from store:{" "}
+        </Typography>
+      </Paper>
     </div>
   );
 }
