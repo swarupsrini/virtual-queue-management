04107617c71dd2a3152aeb0279056d4541180848
jasonconte999 <39284732+jasonconte999@users.noreply.github.com>
2020-08-07 03:40:39 -0400

    Adjusted styles of store header again

--- client/src/components/StoreHeader/styles.js
+++ client/src/components/StoreHeader/styles.js
@@ -2,15 +2,12 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles({
   title: {
-    marginTop: "20px",
-    marginLeft: "30px",
     textAlign: "left",
     fontSize: "45pt",
     color: "#4455BB",
   },
   subtitle: {
     textAlign: "left",
-    marginLeft: "30px",
     fontSize: "14pt",
   },
   parent: {
