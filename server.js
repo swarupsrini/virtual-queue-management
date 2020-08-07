ba7aaf810dae4fa60b211ccff92df96a332b36d6
Hemant Bhanot <hemant.bhanot01@gmail.com>
2020-08-07 13:20:59 -0400

    built server

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const app = express();
 // Setting up a static directory for the files in /pub
 // using Express middleware.
 // Don't put anything in /pub that you don't want the public to have access to!
-app.use(express.static(path.join(__dirname, "client/public")));
+app.use(express.static(__dirname + "/client/build"));
+
+// All routes other than above will go to index.html
 app.get("*", (req, res) => {
   // check for page routes that we expect in the frontend to provide correct status code.
   // const goodPageRoutes = ["/", "/login", "/dashboard"];
@@ -21,19 +23,12 @@ app.get("*", (req, res) => {
   // }
 
   // send index.html
-  res.sendFile(path.join(__dirname + "/client/public/index.html"));
-  res.end();
+  res.sendFile(__dirname + "/client/build/index.html");
 });
 
-// Let's make some express 'routes'
-// Express has something called a Router, which
-// takes specific HTTP requests and handles them
-// based on the HTTP method and URL
-
-// Let's make a route for an HTTP GET request to the
-
-// will use an 'environmental variable', process.env.PORT, for deployment.
+/*************************************************/
+// Express server listening...
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   log(`Listening on port ${port}...`);
-}); // localhost development port 5000  (http://localhost:5000)
+});
