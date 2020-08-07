dfb9435ca39c6a7d9e8b26b2f9988aae9202739f
Hemant Bhanot <hemant.bhanot01@gmail.com>
2020-08-07 02:26:47 -0400

    edited store header

--- client/src/components/StoreHeader/styles.js
+++ client/src/components/StoreHeader/styles.js
@@ -2,15 +2,15 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles({
   title: {
-    marginTop: "10px",
-    marginLeft: "20px",
+    marginTop: "20px",
+    marginLeft: "30px",
     textAlign: "left",
-    fontSize: "50pt",
+    fontSize: "45pt",
     color: "#4455BB",
   },
   subtitle: {
     textAlign: "left",
-    marginLeft: "10px",
+    marginLeft: "30px",
     fontSize: "14pt",
   },
   parent: {
