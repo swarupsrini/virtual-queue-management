26340c598da32c619fb8b0327c7752fe6ed997b8
Swarup Srini <gswarup2001@gmail.com>
2020-08-07 13:21:04 -0400

    Did more queue dashboard work

--- client/src/pages/QueueDashboardPage/index.js
+++ client/src/pages/QueueDashboardPage/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
 
 import useInterval from "../../utils/useInterval";
 import { REFRESH_INTERVAL, getUserStore } from "../../utils/actions";
@@ -12,7 +13,14 @@ import useStyles from "./styles";
 function InfoCard(props) {
   return (
     <div className={props.classes.infoDiv}>
-      <ShoppingCartIcon htmlColor="white"></ShoppingCartIcon>
+      {props.icon === "shop" ? (
+        <ShoppingCartIcon className={props.classes.infoIcon}></ShoppingCartIcon>
+      ) : (
+        <PeopleAltIcon className={props.classes.infoIcon}></PeopleAltIcon>
+      )}
+
+      <p className={props.classes.infoTitle}>{props.title}</p>
+      <p className={props.classes.infoSubtitle}>{props.subtitle}</p>
     </div>
   );
 }
@@ -34,7 +42,18 @@ export default function QueueDashboard(props) {
         subtitle="300 Borough Dr Unit 3635, Scarborough, ON M1P 4P5"
       ></StoreHeader>
       <div className={classes.topRightDiv}>
-        <InfoCard classes={classes}></InfoCard>
+        <InfoCard
+          classes={classes}
+          icon="shop"
+          title="45"
+          subtitle="In store"
+        ></InfoCard>
+        <InfoCard
+          classes={classes}
+          icon="person"
+          title="45"
+          subtitle="In queue"
+        ></InfoCard>
       </div>
     </div>
   );
