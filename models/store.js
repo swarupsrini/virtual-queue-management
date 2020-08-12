0308b9e0dee2a3083810f6b5446046fa952653ad
jasonconte999 <39284732+jasonconte999@users.noreply.github.com>
2020-08-12 02:07:27 -0400

    Updated store model to expand open time to an hours object, which maps days of the week to their open and close times at that day

--- models/store.js
+++ models/store.js
@@ -7,7 +7,7 @@ const Store = mongoose.model('Store', {
 	num_people_in_store: {type: Number},
 	customer_visits: {type: Array},
 	user_entry_rate: {type: Array},
-	open_time: {type: Date},
+	hours: {type: Object},
 	queue: {type: Array},
 	verified: {type: Boolean},
 	creator_id: {type: String},
